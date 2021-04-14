d7152f496050144c98b6f97a201ea8c252b26615
ldofst10 <leaderofst10@gmail.com>
2021-04-14 22:17:35 +0900

    'h'

--- crawler/index.js
+++ crawler/index.js
@@ -1,8 +1,22 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const mysql = require('mysql');
 
-//let
-axios.get('http://210.125.122.79/webseat/roomview5.asp?room_no=6')
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.dbHost,
+    user: process.env.dbUser,
+    password: process.env.dbPwd,
+    database : process.env.dbDb
+});
+connection.connect(function(err) {
+    if(err) {
+        console.error(err); 
+        process.exit(1);
+    }
+
+    axios.get('http://210.125.122.79/webseat/roomview5.asp?room_no=6')
     .then( dataa => {
         const $ = cheerio.load(dataa.data);
         //$('section.book-toc>ul>li>a').each((index, item)=>{$href.push(item.attribs.href)});
@@ -10,11 +24,26 @@ axios.get('http://210.125.122.79/webseat/roomview5.asp?room_no=6')
         for(let i = 1; i < 90; i++) {
             text += i+':' + $('div#Layer'+ i +' table tr td').attr('bgcolor') + '\n';
         }
-        //db에서 5초전에 파싱한 텍스트 불러옴
-        //없으면 text 등록
+        //console.log(text);
+        connection.query('SELECT text FROM textLogs where ReadingRoomId=1;', (err, results, fields) => {
+            if(err) { 
+                console.log(err);process.exit(1); }
+            else if (results.length === 0) connection.query('INSERT INTO textLogs(ReadingRoomId,text) VALUES (1,\''+ text +'\');');
+            else {
+                connection.query('UPDATE textlogs SET text = \''+ text +'\' WHERE ReadingRoomId=1;');
+                console.log('abc');
+            }  
+        });
+        //connection.query('INSERT INTO textLogs(ReadingRoomId,text) VALUES (1,\''+ text +'\');');
         //있으면 가져와서 비교, 같으면 넘어감
         //다르면 새 text로 db UPDATE하고 
         //기존 새 text 비교해서 달라진 좌석 구별
         //구별된 자석들 DB에 색깔과 함께 로그 생성
-        console.log(text);
     })
+    .catch( err=> {
+        connection.end();
+        process.exit(1);
+    });
+});
+
+//connection.end();
--- frontend/README.md
+++
@@ -1,4 +0,0 @@
-# 리바이조 프론트
-## created by react.js
-* ts적용
-* react-bootstrap
