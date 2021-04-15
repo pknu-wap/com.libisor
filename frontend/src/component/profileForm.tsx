6d19f0c832010de730fa1d8977b41668a7d4e11f
hyeonseungKang <khskeb0513@pukyong.ac.kr>
2021-04-15 09:49:30 +0900

    profile-logout form init

---
+++ frontend/src/component/profileForm.tsx
@@ -0,0 +1,35 @@
+import React, {Dispatch, SetStateAction} from "react";
+import {Button, Col, Container, Row} from "react-bootstrap";
+import Common from "./common";
+
+interface ProfileFormProps {
+    setUsername: Dispatch<SetStateAction<string | null>>
+    setProfileFormVisible: Dispatch<SetStateAction<boolean>>
+}
+
+const ProfileForm: React.FC<ProfileFormProps> = ({setProfileFormVisible, setUsername}) => {
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col>
+                        <Button variant={"outline-danger"} onClick={() => {
+                            setUsername(null)
+                            setProfileFormVisible(false)
+                        }}>
+                            로그아웃
+                        </Button>
+                    </Col>
+                </Row>
+                <Common.Blank/>
+                <Row>
+                    <Col>
+                        <hr/>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default ProfileForm
