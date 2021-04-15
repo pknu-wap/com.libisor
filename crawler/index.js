528c9a7785267bcd6f59bff1dba3ffee53b932dc
ldofst10 <leaderofst10@gmail.com>
2021-04-16 00:35:57 +0900

    crwlr dbinit

--- crawler/index.js
+++ crawler/index.js
@@ -28,20 +28,23 @@ axios.get('http://210.125.122.79/webseat/roomview5.asp?room_no=6')
        connection.query('SELECT text FROM textLogs where ReadingRoomId=1;', (err, results, fields) => {
             if(err) process.exit(1);
             else if (results.length === 0) connection.query('INSERT INTO textLogs(ReadingRoomId,text) VALUES (1,\''+ text +'\');');
-            //else if(text===results[0].text) ;
-            else { /*(text!==results[0].text)*/ 
-                connection.query('UPDATE textlogs SET text = \''+ text +'\' WHERE ReadingRoomId=1;');
-                const newLog = text.split('\n');
-                const oldLog = results[0].text.split('\n');
-                newLog.pop();
-                oldLog.pop();
-                let changeLog = {};
-                for ( log in newLog ) {
-                    if(newLog[log] === oldLog[log]) { // !== 로 바꾸면 달라진거 구별됨
-                        changeLog[(1+parseInt(log))] = newLog[log].split(':')[1];
+            //else if(text===results[0].text) ; // 텍스트가 같으면 람다 종료, 테스트용으로 주석 처리
+            else { // (text!==results[0].text) 텍스트가 다르면 달라진 좌석 찾아서 처리
+                connection.query('UPDATE textlogs SET text = \''+ text +'\' WHERE ReadingRoomId=1;', (err, res, fld) => {
+                    const newLog = text.split('\n');
+                    const oldLog = results[0].text.split('\n');
+                    newLog.pop(); // 90번 제거
+                    oldLog.pop();
+                    let changeLog = {};
+                    for ( log in newLog ) {
+                        if(newLog[log] === oldLog[log]) { // !== 로 바꾸면 달라진거 구별됨(테스트용으로 ===)
+                            changeLog[(1+parseInt(log))] = newLog[log].split(':')[1];
+                        }
                     }
-                }
-                console.log(changeLog);
+                    console.log(changeLog);
+                    console.log('DONE..');
+                    connection.end();
+                });
             }
         });
     })
@@ -49,5 +52,3 @@ axios.get('http://210.125.122.79/webseat/roomview5.asp?room_no=6')
         connection.end();
         process.exit();
     });
-
-setTimeout(function () {connection.end(); process.exit()},1000);
--- dbinit/index.js
+++ dbinit/index.js
@@ -7,21 +7,44 @@ const conn = mysql.createConnection({
     user: process.env.dbUser,
     password: process.env.dbPwd,
     database : process.env.dbDb
-})
-conn.connect( async function (err) {
+});
+conn.connect( function (err) {
     if (err) {
         console.error(err);
         process.exit();
     }
 });
 
-conn.query('INSERT INTO readingrooms(name) VALUES (\'CNTMRRNTB\');', (err, res, fld) => {
-    console.error(err);
-});
-for(let i = 1; i <= 89; i++ ) {
-    conn.query('INSERT INTO seats(seatNumber, seatedOrNot, ReadingRoomId) VALUES ('+i+'0,1);', (err, res, fld) => {
-        console.error(err);
-    });
-}
 
-setTimeout(function(){conn.end();}, 3000);
+conn.query('delete from seats;', (err, res, fld) => {
+    if(err) console.error(err);
+    else {
+        conn.query('delete from readingrooms;', (err, res, fld) => {
+            if(err) console.error(err);
+            else
+            conn.query('alter table readingrooms auto_increment=1;', (err, res, fld) => {
+                if(err) console.error(err);
+                else 
+                conn.query('alter table seats auto_increment=1;', (err, res, fld) => {
+                    if(err) console.error(err);
+                    else
+                    conn.query('INSERT INTO readingrooms(name) VALUES (\'CNTMRRNTB\');', (err, res, fld) => {
+                        if(err) console.error(err);
+                        else {
+                            let i;
+                        for(i = 1; i <= 89; i++ ) {
+                            conn.query('INSERT INTO seats(seatNumber, seatedOrNot, ReadingRoomId) VALUES ('+i+',0,1);', (err, res, fld) => {
+                                if(err) console.error(err);
+                            });
+                        }
+                        if (i===90) {
+                                console.log('DONE..'); 
+                                conn.end();
+                        }
+                        }
+                    });
+                });
+            });
+        });
+    }
+});
