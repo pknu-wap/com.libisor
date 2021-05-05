c653a76a5fb437fdf462783bcb64611df20d67d7
ldofst10 <leaderofst10@gmail.com>
2021-05-06 00:48:13 +0900

    0506

--- crawler/index.js
+++ crawler/index.js
@@ -20,28 +20,43 @@ connection.connect( function(err) {
 
 axios.get('http://210.125.122.79/webseat/roomview5.asp?room_no=6')
     .then( dataa => {
+        /* 좌석 사이트 스크래핑 해오기 */
         const $ = cheerio.load(dataa.data);
         let text = '';
         for(let i = 1; i < 90; i++) {
             text += i+':' + $('div#Layer'+ i +' table tr td').attr('bgcolor') + '\n';
         }
+        // 이전에 스크래핑한 내용 가져오기
        connection.query('SELECT text FROM textLogs where ReadingRoomId=1;', (err, results, fields) => {
-            if(err) process.exit(1);
-            else if (results.length === 0) connection.query('INSERT INTO textLogs(ReadingRoomId,text) VALUES (1,\''+ text +'\');');
+            if(err) process.exit(1); 
+            else if (results.length === 0) {
+                connection.query('INSERT INTO textLogs(ReadingRoomId,text) VALUES (1,\''+ text +'\');', () => {
+                    console.log('DONE');
+                    connection.end();
+                });
+            }
             //else if(text===results[0].text) ; // 텍스트가 같으면 람다 종료, 테스트용으로 주석 처리
             else { // (text!==results[0].text) 텍스트가 다르면 달라진 좌석 찾아서 처리
+                // 달라진 텍스트 로그 DB에 업데이트
                 connection.query('UPDATE textlogs SET text = \''+ text +'\' WHERE ReadingRoomId=1;', (err, res, fld) => {
-                    const newLog = text.split('\n');
-                    const oldLog = results[0].text.split('\n');
-                    newLog.pop(); // 90번 제거
-                    oldLog.pop();
+                    const newLog = text.split('\n',89);
+                    const oldLog = results[0].text.split('\n',89);
                     let changeLog = {};
-                    for ( log in newLog ) {
+                    // 새 로그 기존 로그 대조, 달라진 좌석 changeLog 객체에 추가
+                    for ( log in newLog ) { 
                         if(newLog[log] === oldLog[log]) { // !== 로 바꾸면 달라진거 구별됨(테스트용으로 ===)
                             changeLog[(1+parseInt(log))] = newLog[log].split(':')[1];
                         }
                     }
-                    console.log(changeLog);
+                    let tor;
+                    for ( log in changeLog ) { 
+                        if ( changeLog[log] === 'red') tor = 1;
+                        else tor = 0;
+                        connection.query(`
+                        INSERT INTO seatRecords(takeOrReturn, createdAt, updatedAt, SeatId) VALUES (${tor}, NOW(), NOW() ,${log});
+                        `);
+                    }
+                    //console.log(changeLog);
                     console.log('DONE..');
                     connection.end();
                 });
--- dbinit/index.js
+++ dbinit/index.js
@@ -15,28 +15,42 @@ conn.connect( function (err) {
     }
 });
 
-
-conn.query('delete from seats;', (err, res, fld) => {
+//좌석 삭제
+conn.query('delete from seats;', (err, res, fld) => { 
     if(err) console.error(err);
     else {
+        // 열람실 삭제
         conn.query('delete from readingrooms;', (err, res, fld) => {
             if(err) console.error(err);
             else
+            conn.query('delete from textlogs', (err, res, fld) => {
+            conn.query('delete from seatRecords', (err, res, fld) => {
+            // 열람실 번호 1번 부터 부여
             conn.query('alter table readingrooms auto_increment=1;', (err, res, fld) => {
                 if(err) console.error(err);
                 else 
+                // 번호 1번 부터 부여
+                conn.query('alter table seatRecords auto_increment=1;', (err, res, fld) => {
                 conn.query('alter table seats auto_increment=1;', (err, res, fld) => {
                     if(err) console.error(err);
                     else
+                    // 중앙 미래로 노트북 열람실 추가
                     conn.query('INSERT INTO readingrooms(name) VALUES (\'CNTMRRNTB\');', (err, res, fld) => {
                         if(err) console.error(err);
                         else {
                             let i;
-                        for(i = 1; i <= 89; i++ ) {
+                        for(i = 1; i <= 89; i++ ) { // 좌석 1 ~ 89번 추가
                             conn.query('INSERT INTO seats(seatNumber, seatedOrNot, ReadingRoomId) VALUES ('+i+',0,1);', (err, res, fld) => {
                                 if(err) console.error(err);
                             });
                         }
+                        // 최초 기본 레코드 주입
+                         for (i =1; i<= 89; i++) { 
+                        conn.query(`
+                        INSERT INTO seatRecords(takeOrReturn, createdAt, updatedAt, SeatId) VALUES (0 , NOW(), NOW() ,${i});
+                        `);
+                        }
+                        //
                         if (i===90) {
                                 console.log('DONE..'); 
                                 conn.end();
@@ -45,6 +59,9 @@ conn.query('delete from seats;', (err, res, fld) => {
                     });
                 });
             });
+            });
+        });
+        });
         });
     }
 });
