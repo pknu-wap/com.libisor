2f29417dec4450655877d4f52df85052d9b4e04d
ldofst10 <leaderofst10@gmail.com>
2021-05-10 00:28:54 +0900

    db

---
+++ dbinit/dbinit.sql
@@ -0,0 +1,27 @@
+-- 삭제 파트
+delete from seatRecords; -- 좌석 기록
+delete from textlogs; -- 스크래핑 텍스트
+delete from seats; -- 좌석
+delete from readingrooms; -- 열람실
+
+-- 속성 파트
+alter table seats auto_increment=1;
+alter table readingrooms auto_increment=1;
+alter table textlogs auto_increment=1;
+alter table seatRecords auto_increment=1;
+
+-- 삽입 파트
+INSERT INTO readingrooms (name) VALUES ('CNTMRRNTB'); -- 중앙 미래로 노트
+
+    DECLARE i INT DEFAULT 1;
+    WHILE (i <= 89) DO -- 미래로 노트북 좌석 추가
+        INSERT INTO seats (seatNumber, ReadingRoomId) VALUES (i, 1);
+        SET i = i + 1; 
+    END WHILE;
+
+    DECLARE i INT DEFAULT 1;
+    WHILE (i <= 89) DO -- 미래로 노트북 기본 레코드 추가
+        INSERT INTO seatRecords (takeOrReturn, SeatId) VALUES (0 , i);
+        SET i = i + 1; 
+    END WHILE;
+-- 크롤링 텍스트 로그는 js에서 텍스트 생성해서 저장
--- dbinit/index.js
+++ dbinit/index.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-
+const path = require('path');
 require('dotenv').config();
 
 const conn = mysql.createConnection({
@@ -15,60 +15,17 @@ conn.connect( function (err) {
     }
 });
 
+var qry = 'SOURCE ' + path.normalize(process.cwd()) + path.sep + 'dbinit.sql';
+
 //좌석 삭제
-conn.query('delete from seats;', (err, res, fld) => { 
+conn.query( qry , (err, res, fld) => { 
     if(err) console.error(err);
-    else {
-        // 열람실 삭제
-        conn.query('delete from readingrooms;', (err, res, fld) => {
-            if(err) console.error(err);
-            else
-            conn.query('delete from textlogs', (err, res, fld) => {
-            conn.query('delete from seatRecords', (err, res, fld) => {
-            // 열람실 번호 1번 부터 부여
-            conn.query('alter table readingrooms auto_increment=1;', (err, res, fld) => {
-                if(err) console.error(err);
-                else 
-                // 번호 1번 부터 부여
-                conn.query('alter table seatRecords auto_increment=1;', (err, res, fld) => {
-                conn.query('alter table seats auto_increment=1;', (err, res, fld) => {
-                    if(err) console.error(err);
-                    else
-                    // 중앙 미래로 노트북 열람실 추가
-                    conn.query('INSERT INTO readingrooms(name) VALUES (\'CNTMRRNTB\');', (err, res, fld) => {
-                        if(err) console.error(err);
-                        else {
-                            let i;
-                             //textLogs 모두 red로 초기화
-                            let text = '';
-                              for(i = 1; i < 90; i++) 
-                                   text += `${i}:gray\n`;
-                            conn.query(`INSERT INTO textlogs(text, readingroomId) VALUES (\'${text}\', 1)`, (err, res, fld) => {
-                                 if(err) console.error(err);
-                            });
-                        for(i = 1; i <= 89; i++ ) { // 좌석 1 ~ 89번 추가
-                            conn.query('INSERT INTO seats(seatNumber, seatedOrNot, ReadingRoomId) VALUES ('+i+',0,1);', (err, res, fld) => {
-                                if(err) console.error(err);
-                            });
-                        }
-                        // 최초 기본 레코드 주입
-                         for (i =1; i<= 89; i++) { 
-                        conn.query(`
-                        INSERT INTO seatRecords(takeOrReturn, createdAt, updatedAt, SeatId) VALUES (0 , NOW(), NOW() ,${i});
-                        `);
-                        }
-                        //
-                        if (i===90) {
-                                console.log('DONE..'); 
-                                conn.end();
-                        }
-                        }
-                    });
-                });
-            });
-            });
-        });
-        });
-        });
-    }
+    //textLogs 모두 gray로 초기화
+    let text = '';
+    for(i = 1; i < 90; i++) 
+         text += `${i}:gray\n`;
+    conn.query(`INSERT INTO textlogs(text, readingroomId) VALUES (\'${text}\', 1)`, (err, res, fld) => {
+       if(err) console.error(err);
+       conn.end();
+  });
 });
