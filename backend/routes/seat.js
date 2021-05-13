02bfaad1ef7204648a000abcfa153d45c69821ac
ldofst10 <leaderofst10@gmail.com>
2021-05-14 00:53:25 +0900

    a

--- backend/routes/seat.js
+++ backend/routes/seat.js
@@ -3,15 +3,16 @@ const { ReadingRoom, Seat, SeatRecord } = require('../models');
 
 const router = express.Router();
 
-router.get('/:readingRoom/(:seatNumber)?', async (req, res) => {
+// '/:readingRoom/(:seatNumber)?'
+router.get('/:readingRoom', async (req, res) => {
     try{
-        let seatNum;
+       /* let seatNum;
     if(req.params.seatNumber == null)
         seatNum = 0;
-    else seatNum = parseInt(req.params.seatNumber);
+    else seatNum = parseInt(req.params.seatNumber); */
     // 중앙 미래로 노트북 id 1 (열람실 id는 DB 접근 X)
     if(req.params.readingRoom==='CNTMRRNTB') {
-        if(seatNum >=1 && seatNum <= 89) {
+        /*if(seatNum >=1 && seatNum <= 89) {
             const record = await Seat.findAll({
                 limit: 1,
                 where: {
@@ -26,10 +27,13 @@ router.get('/:readingRoom/(:seatNumber)?', async (req, res) => {
                 }
             });
             res.send(record);
-        } else {
+        } else { */
             const record = await Seat.findAll({
                 where: {
                     ReadingRoomId: 1,
+                },
+                attributes: {
+                    exclude: ['ReadingRoomId']
                 },
                 include: {
                     model: SeatRecord,
@@ -38,8 +42,17 @@ router.get('/:readingRoom/(:seatNumber)?', async (req, res) => {
                     order: [ [ 'createdAt', 'DESC' ]]
                 }
             });
-            res.send(record);
-        }
+            let result = [];
+            result.push({});
+            await record.forEach(e => {
+                let data = {};
+                data["seatNumber"] = e.seatNumber;
+                data["taken"] = e.SeatRecords[0].takeOrReturn;
+                data["time"] = e.SeatRecords[0].createdAt.toISOString().replace('.000Z','');
+                result.push(data);
+            });
+            res.json(result);
+       // }
     }
 } catch (error) {
     console.error(error);
