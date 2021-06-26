568a3baf2cd500b6093667ab2b652db8ac0bdaf0
hyeonseungKang <khskeb0513@pukyong.ac.kr>
2021-06-26 15:43:37 +0900

    404에러 처리 라우터 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import NavBar from "./component/navBar";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Common from './component/common';
@@ -6,6 +6,8 @@ import IndexForm from "./component/indexForm";
 import BeforeLoginForm from "./component/beforeLoginForm";
 import ProfileForm from "./component/profileForm";
 import FooterForm from "./component/footerForm";
+import UserService from "./service/user/userService";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
     const [username, setUsername] = useState<string | null>(null)
@@ -13,8 +15,13 @@ function App() {
     const [isLoginForm, setIsLoginForm] = useState<boolean>(false)
     const [profileFormVisible, setProfileFormVisible] = useState<boolean>(false)
     const [libName, setLibName] = useState<string>('mireaB1')
+    useEffect(() => {
+        UserService.getUsername().then(r => {
+            setUsername(r)
+        })
+    })
     return (
-        <div>
+        <BrowserRouter>
             <NavBar
                 setBeforeLoginFormVisible={setBeforeLoginFormVisible}
                 setIsLoginForm={setIsLoginForm}
@@ -22,17 +29,25 @@ function App() {
                 setProfileFormVisible={setProfileFormVisible}
                 username={username}
                 setLibName={setLibName}/>
-            <Common.Blank/>
-            {!username && beforeLoginFormVisible
-                ? <BeforeLoginForm setBeforeLoginFormVisible={setBeforeLoginFormVisible} setUsername={setUsername}
-                                   isLoginForm={isLoginForm} setIsLoginForm={setIsLoginForm}/>
-                : null}
-            {username && profileFormVisible
-                ? <ProfileForm setProfileFormVisible={setProfileFormVisible} setUsername={setUsername}/>
-                : null}
-            <IndexForm username={username} libName={libName}/>
+            <Switch>
+                <Route exact path={'/'}>
+                    <Common.Blank/>
+                    {!username && beforeLoginFormVisible
+                        ? <BeforeLoginForm setBeforeLoginFormVisible={setBeforeLoginFormVisible}
+                                           setUsername={setUsername}
+                                           isLoginForm={isLoginForm} setIsLoginForm={setIsLoginForm}/>
+                        : null}
+                    {username && profileFormVisible
+                        ? <ProfileForm setProfileFormVisible={setProfileFormVisible} setUsername={setUsername}/>
+                        : null}
+                    <IndexForm username={username} libName={libName}/>
+                </Route>
+                <Route path={'*'}>
+                    404
+                </Route>
+            </Switch>
             <FooterForm/>
-        </div>
+        </BrowserRouter>
     )
 }
 
